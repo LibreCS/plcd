ea9fd69873ad32419709d098df9f871c817221cf
Christian Strommen <cpstrommen@gmail.com>
2022-07-29 20:03:37 -0500

    removed src dir

--- plcd.py
+++ plcd.py
@@ -1,4 +1,5 @@
-from src.plcd.ping import main
+from plcd.ping import main
 
 if __name__ == '__main__':
+    
     main()
--- plcd/__main__.py
+++ plcd/__main__.py
@@ -1,4 +1,5 @@
 # plcd.__main__: executed when bootstrap directory is called as a script
 
 from .ping import main
+
 main()
