731bf2cf441dc2df9296242491813da716ef956c
Christian Strommen <cpstrommen@gmail.com>
2022-08-01 12:48:26 -0500

    Update test-sniff.py

--- tests/test-sniff.py
+++ tests/test-sniff.py
@@ -1,15 +1,22 @@
 
 import unittest
 from src.plcd.ping import portSniff
+from src.plcd.ping import portIngest
+from src.plcd.ping import PLC_PORT_DEFINITIONS
 
 SNIFF_TEST_IP = "185.199.109.153"
 SNIFF_TEST_PORT = 443
 
+portName = [str("")]*PLC_PORT_DEFINITIONS
+portNum = [int(0)]*PLC_PORT_DEFINITIONS
+
 class SniffTest(unittest.TestCase):
- 
-    def test_portSniff(self):
+
+    portIngest()
+
+    def test_portIngest(self):
         self.assertEqual(portSniff(SNIFF_TEST_IP), SNIFF_TEST_PORT)
- 
- 
+
+
 if __name__ == '__main__':
     unittest.main()
