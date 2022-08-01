5b5009bb2ef579477a3c7876743a143f52ba24a9
Christian Strommen <cpstrommen@gmail.com>
2022-08-01 11:56:06 -0500

    Update test-sniff.py

--- tests/test-sniff.py
+++ tests/test-sniff.py
@@ -11,7 +11,7 @@ SNIFF_TEST_PORT = 443
 
 class SniffTest(unittest.TestCase):
  
-    def test_portIngest(self):
+    def test_portSniff(self):
         self.assertEqual(portSniff(SNIFF_TEST_IP), SNIFF_TEST_PORT)
  
  
