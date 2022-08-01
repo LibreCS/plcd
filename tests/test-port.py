bc318a5b9c422447150347c361af9a77efaab688
Christian Strommen <cpstrommen@gmail.com>
2022-08-01 11:54:54 -0500

    Add files via upload

---
+++ tests/test-port.py
@@ -0,0 +1,17 @@
+
+import unittest
+from plcd.main import portValid
+
+PORT = [0, 50001, 443]
+PORT_STATUS = [False, False, True]
+
+class PortTest(unittest.TestCase):
+ 
+    def test_portValid(self):
+    
+        for i in PORT:
+            self.assertEqual(portValid(PORT[i]), PORT_STATUS[i])
+
+
+if __name__ == '__main__':
+    unittest.main()
