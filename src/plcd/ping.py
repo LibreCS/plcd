d38642bb1546f0bafe89bf58d5987d8c5287edae
Christian Strommen <cpstrommen@gmail.com>
2022-08-01 14:03:23 -0500

    version

--- src/plcd/ping.py
+++ src/plcd/ping.py
@@ -6,7 +6,7 @@ import socket
 import datetime
 import time
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 
 #=========== CONFIGURATION ===========================================
 # set monitoring frequency (pings/min)
